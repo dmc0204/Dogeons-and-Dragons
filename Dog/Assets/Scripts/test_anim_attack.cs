e63aa611bb394390b452a0f81c60131fe5fa5da2
John J <7483411+jjakkc@users.noreply.github.com>
2019-10-16 21:56:54 -0400

    Added new units with animations for idle & attack

---
+++ Dog/Assets/Scripts/test_anim_attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_anim_attack : MonoBehaviour
+{
+    private Animator anim;
+
+    private bool isAttacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isAttacking = true;
+            Debug.Log("Pressed attack");
+            //anim.SetBool("isAttacking", true);
+            anim.SetTrigger("swing");
+        }
+        //else
+        //{
+        //    isAttacking = false;
+        //    //anim.SetBool("isAttacking", false);
+        //}
+    }
+}
