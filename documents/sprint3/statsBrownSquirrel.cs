b89fee8ae8a3935bffe5cae37bfd6c0a780d2c0f
austinkue <supersalamandar@gmail.com>
2019-10-16 23:06:19 -0400

    austin's fact's

---
+++ documents/sprint3/statsBrownSquirrel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statsBrownSquirrel : MonoBehaviour
+{
+    public FloatReference MaxHP;
+    public FloatReference CurrentHP;
+    public FloatReference Speed;
+    public FloatReference Attack;
+    public FloatReference Defense;
+}
