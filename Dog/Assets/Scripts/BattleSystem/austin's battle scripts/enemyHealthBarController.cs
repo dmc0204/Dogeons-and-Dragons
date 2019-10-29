49cf5ec523970345f545d960ebbfa45bc3aa346a
austinkue <supersalamandar@gmail.com>
2019-10-29 12:53:20 -0400

    myCoolScripts

---
+++ Dog/Assets/Scripts/BattleSystem/austin's battle scripts/enemyHealthBarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemyHealthBarController : MonoBehaviour
+{
+    public Slider bar;
+    public EnemyStatsConfig maxStuff;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
