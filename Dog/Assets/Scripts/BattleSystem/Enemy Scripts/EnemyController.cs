cf3f2da0d7cda43a20aab80c6aeb53d447c10640
austinkue <supersalamandar@gmail.com>
2019-11-10 13:23:11 -0500

    my other shit

--- Dog/Assets/Scripts/BattleSystem/Enemy Scripts/EnemyController.cs
+++ Dog/Assets/Scripts/BattleSystem/Enemy Scripts/EnemyController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
     public EnemyStatsConfig myCoolVariable;
 
     //[System.NonSerialized]
@@ -26,33 +27,42 @@ public class EnemyController : MonoBehaviour {
     //functions to handle enemy death
 
     //Checks if enemy's HP is dead
-    public bool isEnemyDead () {
+    public bool isEnemyDead()
+    {
         if (currentHP > 0)
             return false;
-        else {
+        else
+        {
             return true;
         }
     }
 
     //calls enemy death animation
-    public void died () {
-        enemyDied.Raise ();
+    public void died()
+    {
+        enemyDied.Raise();
         //TODO:write something in animator that links this call to the
         //wantNewEnemy call.  that way the new enemy only spawns when the 
         //old one's death animation finishes
     }
 
     //clears enemy sprites
-    void nullEnemy () {
+    void nullEnemy()
+    {
         enemyArmSpriteRenderer.sprite = null;
         enemyBodySpriteRenderer.sprite = null;
+        currentAttack = 0;
+        currentDefense = 0;
+        currentSpeed = 0;
+        currentHP = -1;
         name.text = "";
     }
 
     //asks if there are enemies left
-    public void wantNewEnemy () {
-        giveMeEnemy.Raise ();
-        Debug.Log ("want new enemy event triggered");
+    public void wantNewEnemy()
+    {
+        giveMeEnemy.Raise();
+        Debug.Log("want new enemy event triggered");
     }
 
     // // // // // // // // // // // // //
@@ -60,20 +70,22 @@ public class EnemyController : MonoBehaviour {
     //functions to handle enemy spawn
 
     //load new enemy
-    public void loadEnemy (EnemyStatsConfig poo) {
+    public void loadEnemy(EnemyStatsConfig poo)
+    {
         myCoolVariable = poo;
-        initializeEnemy ();
+        initializeEnemy();
     }
 
     //initialize values based off of currently loaded enemy
-    public void initializeEnemy () {
+    public void initializeEnemy()
+    {
         currentHP = myCoolVariable.MaxHealth;
         currentAttack = myCoolVariable.BaseAttack;
         currentDefense = myCoolVariable.BaseDefense;
         currentSpeed = myCoolVariable.BaseSpeed;
         enemyArmSpriteRenderer.sprite = myCoolVariable.arm;
         enemyBodySpriteRenderer.sprite = myCoolVariable.body;
-        enemyAnimator = GetComponent<Animator> ();
+        enemyAnimator = GetComponent<Animator>();
         enemyAnimator.runtimeAnimatorController = myCoolVariable.animator;
         name.text = myCoolVariable.enemyName;
         timeAbleToBasicAttack = Time.time;
@@ -82,62 +94,72 @@ public class EnemyController : MonoBehaviour {
     // // // // // // // // // // // //
 
     //healthbar controller functions
-    public void updateHealthBar () {
-        healthbar.value = calcHealth ();
+    public void updateHealthBar()
+    {
+        healthbar.value = calcHealth();
     }
 
-    public float calcHealth () {
+    public float calcHealth()
+    {
         return currentHP / myCoolVariable.MaxHealth;
     }
     // // // // // // // // // // // //
 
     //START: battle functions
-    public void calculateDamageFromAttack (float attack) {
-        System.Random rand = new System.Random ();
-        double dubmult = rand.NextDouble () + 1;
+    public void calculateDamageFromAttack(float attack)
+    {
+        System.Random rand = new System.Random();
+        double dubmult = rand.NextDouble() + 1;
 
-        float multiplier = (float) dubmult;
+        float multiplier = (float)dubmult;
         //Debug.Log (multiplier);
         float damage = (attack * multiplier) - currentDefense;
         if (damage < 0)
             damage = 1;
-        takeDamage (damage);
+        takeDamage(damage);
         //TODO: tell level how much damage was taken
     }
 
-    public void takeDamage (float damage) {
+    public void takeDamage(float damage)
+    {
         currentHP -= damage;
         //updateHealthBar ();
     }
 
     //enemy autoattack cooldown
-    public float basicAttackCooldown () {
+    public float basicAttackCooldown()
+    {
         return 10 / currentSpeed;
     }
 
     //enemy autoattack
-    void enemyAttack () {
-        if (Time.time > timeAbleToBasicAttack) {
-            timeAbleToBasicAttack = Time.time + basicAttackCooldown ();
-            enemyAttacking.Raise (currentAttack);
-            enemyAnimator.SetTrigger ("attack");
+    void enemyAttack()
+    {
+        if ((Time.time > timeAbleToBasicAttack) && (!isEnemyDead()))
+        {
+            timeAbleToBasicAttack = Time.time + basicAttackCooldown();
+            enemyAttacking.Raise(currentAttack);
+            enemyAnimator.SetTrigger("attack");
         }
     }
 
     // // // // // // /// // // // // //
 
     //start and update
-    void Start () {
-        wantNewEnemy ();
+    void Start()
+    {
+        wantNewEnemy();
     }
 
-    void Update () {
-        enemyAttack ();
-        updateHealthBar ();
-        if (isEnemyDead ()) {
-            died ();
-            nullEnemy ();
-            wantNewEnemy ();
+    void Update()
+    {
+        enemyAttack();
+        updateHealthBar();
+        if (isEnemyDead())
+        {
+            died();
+            nullEnemy();
+            wantNewEnemy();
         }
     }
 }
--- Dog/Assets/Scripts/BattleSystem/Level Scripts/LevelController.cs
+++ Dog/Assets/Scripts/BattleSystem/Level Scripts/LevelController.cs
@@ -2,63 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelController : MonoBehaviour {
+public class LevelController : MonoBehaviour
+{
     private float damageTaken, damageDealt;
     //TODO:funcs to track these, get passed numbers
 
     private bool dogDied;
 
-    public int count;
+    //public int count;
     public LevelConfig newLevel;
     public EnemyStatsConfig[] enemies;
+
+    public Queue<EnemyStatsConfig> NMEs;
     public DogStatsConfig[] dogs;
+
     public Sprite background;
     public SpriteRenderer backgroundSpriteRenderer;
     [SerializeField] private EnemyStatsEvent gettingEnemyStats;
 
     //initializes level values
-    public void initialize () {
+    public void initialize()
+    {
         damageTaken = 0;
         damageDealt = 0;
-        count = 0;
+        //count = 0;
         enemies = newLevel.enemies;
         dogs = newLevel.yourTeam;
         background = newLevel.background;
-        backgroundSpriteRenderer = GetComponent<SpriteRenderer> ();
+        backgroundSpriteRenderer = GetComponent<SpriteRenderer>();
         backgroundSpriteRenderer.sprite = background;
+    }
+
+    //initializes queue
+    public void initQueue()
+    {
+        NMEs = new Queue<EnemyStatsConfig>(enemies);
     }
 
     //functions to handle spawning new enemies
     //checks if there are enemies left to spawn
-    public bool enemiesIsEmpty () {
-        if (count >= enemies.Length) {
+    public bool enemiesIsEmpty()
+    {
+        if (NMEs.Count == 0)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return false;
         }
     }
 
     //returns next enemy in list
-    public EnemyStatsConfig getNext () {
+    public EnemyStatsConfig getNext()
+    {
         EnemyStatsConfig toReturn;
-        int i = count;
-        toReturn = enemies[i];
-        count++;
+        toReturn = NMEs.Dequeue();
         return toReturn;
     }
 
-    void enemyDied () {
+    void enemyDied()
+    {
         //Debug.Log ("enemydied check triggered");
-        if (!enemiesIsEmpty ()) {
-            gettingEnemyStats.Raise (getNext ());
-        } else {
-            Debug.Log ("its empty!");
-            endLevel ();
+        if (!enemiesIsEmpty())
+        {
+            gettingEnemyStats.Raise(getNext());
+        }
+        else
+        {
+            Debug.Log("its empty!");
+            endLevel();
         }
     }
     // // // // // // // // // // // // // // // // // //
 
-    public void endLevel () {
+    public void endLevel()
+    {
         //TODO:end the level with this function
         //stop time
         //call up level end panel
@@ -68,12 +87,15 @@ public class LevelController : MonoBehaviour {
     }
 
     // Start is called before the first frame update
-    void Start () {
-        initialize ();
+    void Start()
+    {
+        initialize();
+        initQueue();
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
     }
 }
