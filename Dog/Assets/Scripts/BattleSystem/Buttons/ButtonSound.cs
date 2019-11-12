80f43fa3a279ee53862823de205b2504edabdaf8
Loi Huynh <loihuynh@oakland.edu>
2019-11-11 21:04:14 -0500

    Stuff for tuesday
    BUttonSound cs file and other scenes and stuff

---
+++ Dog/Assets/Scripts/BattleSystem/Buttons/ButtonSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Button))]
+public class ButtonSound : MonoBehaviour
+{
+    public AudioClip sound;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source {get{return GetComponent<AudioSource>();}}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+
+        button.onClick.AddListener(() => PlaySound());
+
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
