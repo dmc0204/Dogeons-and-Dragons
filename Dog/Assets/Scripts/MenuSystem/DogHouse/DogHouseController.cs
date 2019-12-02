73c20da6dd527563b5d3bff79e19526448fde5d1
austinkue <supersalamandar@gmail.com>
2019-12-02 18:45:48 -0500

    lots of shit

---
+++ Dog/Assets/Scripts/MenuSystem/DogHouse/DogHouseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DogHouseController : MonoBehaviour
+{
+
+    public PlayerConfig myPlayer;
+
+    public Button[] dogButtons;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Dog/Assets/Scripts/Player/Player Scripts/PlayerConfig.cs
+++ Dog/Assets/Scripts/Player/Player Scripts/PlayerConfig.cs
@@ -1,35 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-[CreateAssetMenu (menuName = "Content/Player Config")]
+[CreateAssetMenu(menuName = "Content/Player Config")]
 
-public class PlayerConfig : ScriptableObject {
+public class PlayerConfig : ScriptableObject
+{
     public DogStatsConfig[] yourTeam;
 
     public ChewableConfig[] yourChewies;
 
     public Dictionary<string, int> inventory;
 
-    public Dictionary<string, bool> dogs;
+    public Dictionary<DogStatsConfig, bool> dogs;
 
-    public void addItems (string key, int value) {
-        if (inventory.ContainsKey (key)) {
+    public void addItems(string key, int value)
+    {
+        if (inventory.ContainsKey(key))
+        {
             inventory[key] += value;
-        } else {
-            inventory.Add (key, value);
+        }
+        else
+        {
+            inventory.Add(key, value);
         }
     }
 
-    public void initializeInventory () {
+
+
+    public void initializeInventory()
+    {
         //TODO: init inventory
     }
 
-    public void usedItem (string key) {
-        if (inventory.ContainsKey (key)) {
+    public void usedItem(string key)
+    {
+        if (inventory.ContainsKey(key))
+        {
             inventory[key] -= 1;
-        } else {
-            Debug.Log ("error used an item you didnt have");
+        }
+        else
+        {
+            Debug.Log("error used an item you didnt have");
         }
     }
 
